d1d5a66ab715bf39979df4b1b8ab41545517c2e9
hyesu-jeong <hyeppy739@gmail.com>
2024-06-27 14:33:53 +0900

    learn.UserCard - post 통신

--- client/main.js
+++ client/main.js
@@ -9,6 +9,7 @@ import {
   changeColor,
   renderSpinner,
   renderEmptyCard,
+  clearContents,
 } from './lib/index.js';
 
 const getData = async () => {
@@ -21,7 +22,7 @@ const getData = async () => {
 
 // getData();
 
-const ENDPOINT = 'https://jsonplaceholder.typicode.com/users';
+const ENDPOINT = 'http://localhost:3000/users';
 
 // 1. user의 data fetch 해주세요.
 // - tiger.get
@@ -38,7 +39,7 @@ async function renderUserList() {
   // 로딩 스피너 렌더링
   renderSpinner(userCardInner);
 
-  await delayP(2000);
+  await delayP(1000);
 
   try {
     gsap.to('.loadingSpinner', {
@@ -80,3 +81,59 @@ renderUserList();
 // form : -부터 / 현재위치 스타일 잡고 여기서부터 모션 시작
 // to : -까지 / 작성한 스타일 잡고 여기까지 완료
 // set : 애니메이션이랑 관련없이 세팅만 해주는 거
+
+function handleDeleteCard(e) {
+  const button = e.target.closest('button');
+
+  if (!button) return;
+
+  const article = button.closest(`article`);
+
+  const index = article.dataset.index.slice(5);
+
+  tiger.delete(`${ENDPOINT}/${index}`).then(() => {
+    // 요청보내고 렌더링하기
+    clearContents(userCardInner);
+    renderUserList();
+  });
+}
+
+userCardInner.addEventListener('click', handleDeleteCard);
+
+/* ----------------- 생성 버튼 만들기 ---------------- */
+
+const createButton = getNode('.create');
+const cancelButton = getNode('.cancel');
+const doneButton = getNode('.done');
+
+function handleCreate() {
+  gsap.to('.pop', { autoAlpha: 1 });
+}
+
+function handleCancel(e) {
+  e.stopPropagation(); // 버블링 때문에 HandleCreate가 같이 작동하는데 그걸 막아주는 코드
+  gsap.to('.pop', { autoAlpha: 0 });
+}
+
+function handleDone(e) {
+  e.preventDefault();
+
+  const name = getNode('#nameField').value;
+  const email = getNode('#emailField').value;
+  const website = getNode('#siteField').value;
+
+  tiger.post(ENDPOINT, { name, email, website }).then(() => {
+    // 1.팝업 닫기
+    gsap.to('.pop', { autoAlpha: 0 });
+
+    // 2. 카드 컨텐츠 비우기
+    clearContents(userCardInner);
+
+    // 3. 유저카드 렌더링하기
+    renderUserList();
+  });
+}
+
+createButton.addEventListener('click', handleCreate);
+cancelButton.addEventListener('click', handleCancel);
+doneButton.addEventListener('click', handleDone);
