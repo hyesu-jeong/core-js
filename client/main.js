7c752bec7d3858b278898416c6dfa1aa843c384d
hyesu-jeong <hyeppy739@gmail.com>
2024-07-03 11:13:50 +0900

    learn.webComponent
    - shadow DOM

--- client/main.js
+++ client/main.js
@@ -1,6 +1,14 @@
-class Button extends HTMLButtonElement {
+class Button extends HTMLElement {
   constructor() {
     super();
+
+    // c-button의 쉐도우 돔 열어줘.
+    this.attachShadow({ mode: 'open' });
+
+    // 그리고 그 안에 내가 원하는 태그 집어 넣을거야
+    this.shadowRoot.innerHTML = /* html */ `
+    <button>hello</button>
+    `;
   }
 
   connectedCallback() {}
@@ -8,4 +16,8 @@ class Button extends HTMLButtonElement {
   disconnectedCallback() {}
 }
 
-customElements.define('c-button', Button, { extends: 'button' });
+customElements.define('c-button', Button);
+
+console.log(
+  document.querySelector('c-button').shadowRoot.querySelector('button')
+);
